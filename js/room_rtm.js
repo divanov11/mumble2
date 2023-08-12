f940685caf5656dc9159d66f91060014f7e47a1d
Sudesha T R <104151479+Sudesha-STR@users.noreply.github.com>
2023-08-13 00:33:31 +0530

    Update room_rtm.js

--- js/room_rtm.js
+++ js/room_rtm.js
@@ -104,7 +104,7 @@ let addBotMessageToDom = (botMessage) => {
 
     let newMessage = `<div class="message__wrapper">
                         <div class="message__body__bot">
-                            <strong class="message__author__bot">🤖 Whisperhub Bot</strong>
+                            <strong class="message__author__bot">🤖 CipherVeil Bot</strong>
                             <p class="message__text__bot">${botMessage}</p>
                         </div>
                     </div>`
